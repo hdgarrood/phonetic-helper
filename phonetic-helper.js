506563f710f73762e9f0672c2bf05c9ad2f415b5
Harry Garrood <harry@garrood.me>
2013-08-02 17:33:46 +0100

    initial commit

---
+++ phonetic-helper.js
@@ -0,0 +1,10 @@
+// MAKE SURE YOU'RE USING UTF8
+
+// This is IPA, RP (as in, proper English).
+// Source: https://en.wiktionary.org/wiki/Appendix:English_pronunciation 
+var phoneticKey = {
+    'æ'     => ['b*a*d', 'c*a*t', 'r*a*n'],
+    'æɹ'    => ['c*arr*y'],
+    'eɪ'    => ['b*ai*t', 'pl*ay*', 's*a*me'],
+    'ɑː'    => ['f*a*ther']
+}

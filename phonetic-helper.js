88d4999148beb614e56c137a91c5ca7ddf1460cb
Harry Garrood <hdgarrood@gmail.com>
2013-08-03 13:39:37 +0100

    stuff

--- phonetic-helper.js
+++ phonetic-helper.js
@@ -1,10 +1,19 @@
 // MAKE SURE YOU'RE USING UTF8
 
 // This is IPA, RP (as in, proper English).
-// Source: https://en.wiktionary.org/wiki/Appendix:English_pronunciation 
+// Source: https://en.wiktionary.org/wiki/Appendix:English_pronunciation
 var phoneticKey = {
-    'æ'     => ['b*a*d', 'c*a*t', 'r*a*n'],
-    'æɹ'    => ['c*arr*y'],
-    'eɪ'    => ['b*ai*t', 'pl*ay*', 's*a*me'],
-    'ɑː'    => ['f*a*ther']
+    'æ'  : ['b*a*d', 'c*a*t', 'r*a*n'],
+    'æɹ' : ['c*arr*y'],
+    'eɪ' : ['b*ai*t', 'pl*ay*', 's*a*me'],
+    'ɑː' : ['f*a*ther']
+}
+
+function renderWord(word) {
+    var parts = word.split('*')
+    return parts[0] + '<em>' + parts[1] + '</em>' + parts[2]
+}
+
+window.onload = function() {
+    $('input#word').inputValue('lol')
 }
